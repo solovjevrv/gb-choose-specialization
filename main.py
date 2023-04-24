d80fe01d1c88e28ea7bdbb2265071878d99ad859
solovjevrv <solovjevrv@gmail.com>
2023-04-24 18:08:00 +0300

    fix program end

--- main.py
+++ main.py
@@ -7,9 +7,11 @@
 # [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 # [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 # [“Russia”, “Denmark”, “Kazan”] → []
-
+import sys
 from modules.func import filter_array
 from modules.request_array import requsetArrayFromUser
 
 input_user_array = requsetArrayFromUser()
 print(filter_array(input_user_array))
+print("Для выхода нажмите Enter")
+print(sys.stdin.readline())
